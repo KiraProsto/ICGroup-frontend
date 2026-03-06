506ce5f969d476aa3da764c6b50411b7d19630f4
Lugovskaya_Kira <kiraprosto0.0@gmail>
2026-03-06 09:15:53 +0300

    add homepage structure and connect global providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { montserrat } from './fonts';
 import { Providers } from './providers';
+import '@mantine/core/styles.css';
 
 export const metadata: Metadata = {
   title: 'IC Group',
