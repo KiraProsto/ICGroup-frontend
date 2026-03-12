ae6bc88cd879e8095972b08ed02aa94c9b456c3b
Lugovskaya_Kira <kiraprosto0.0@gmail>
2026-03-12 11:50:23 +0300

    create mock dataset for news cards

--- src/components/news/NewsCard.tsx
+++ src/components/news/NewsCard.tsx
@@ -1,15 +1,14 @@
-export default function NewsCard() {
+import { INewsItem } from '@/app/press/types';
+
+export default function NewsCard({ item }: { item: INewsItem }) {
   return (
-    <div className="p-6 rounded-lg bg-white shadow-main-3 shadow-md">
-      <p className="inline-block bg-gray-1 rounded-md px-3 py-1.5 text-gray-2 text-base font-medium">
-        Тег
+    <div className="flex flex-col p-6 rounded-lg bg-white shadow-main-2 shadow-md cursor-pointer transition-all duration-200 hover:scale-[0.99]">
+      <p className="self-start bg-gray-1 rounded-md px-3 py-1.5 text-gray-2 text-base font-medium">
+        {item.label}
       </p>
-      <h2 className="py-3 text-lg font-semibold">
-        Название длинное очень по типу: Связь, которая остаётся с вами: «Волна»
-        обеспечила доступ к привычным сервисам в 2025 году
-      </h2>
-      <p className="text-gray-2 text-sm font-medium text-gray-600">
-        00.00.0000
+      <h2 className="py-3 text-lg font-semibold">{item.title}</h2>
+      <p className="text-gray-2 text-sm font-medium text-gray-600 mt-auto">
+        {new Date(item.date).toLocaleDateString('ru-RU')}
       </p>
     </div>
   );
--- src/components/news/NewsList.tsx
+++ src/components/news/NewsList.tsx
@@ -1,13 +1,14 @@
+import { newsMockData } from '@/app/press/NewsMockData';
 import NewsCard from './NewsCard';
 
 export default function NewsList() {
   return (
-    <section className="pt-12 px-5 bg-gray-0">
+    <section className="pt-12 px-5 bg-gray-0 min-h-screen">
       <div className="max-w-screen-xl mx-auto">
         <div className="bg-gray-0 grid gap-8 grid-cols-2 lg:grid-cols-3">
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
+          {newsMockData.map((item) => (
+            <NewsCard key={item.id} item={item} />
+          ))}
         </div>
       </div>
     </section>
